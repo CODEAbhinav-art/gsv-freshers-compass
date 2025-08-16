86a951dc790c9d3b401431cbbf23526115f3799c
Abhinav Mishra <abhinav15102003@gmail.com>
2025-08-16 22:32:02 +0530

    Update BranchwiseMentors.tsx

--- src/pages/BranchwiseMentors.tsx
+++ src/pages/BranchwiseMentors.tsx
@@ -29,7 +29,8 @@ const BranchwiseMentors = () => {
               linkedin: "https://www.linkedin.com/in/abhinavamishra?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
               email: "2005.abhinavmishra@gmail.com"
             }
-          },
+          }
+          ],
           
         females: [
           {
